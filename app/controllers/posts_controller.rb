38c9a063766201096efb1f9880dede6bfc6ef9f6
Michael Glaser <email": null>
2017-08-28 15:12:08 -0700

    Adds posts show functionality.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,13 @@ class PostsController < ApplicationController
   def index
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
   end
+
 
   def create
     # render plain: params[:post].inspect
